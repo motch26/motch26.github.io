ae7e4756cbc3c39cf0c3fb10e2a83b68f7e4ca71
motch26 <motch26@gmail.com>
2023-02-04 10:56:41 +0800

    New Update Test

---
+++ scoreboard/precache-manifest.1c1b16c1f8989260e3dee94686502d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be736e63161cf727fec1b453628b27ca",
+    "url": "/motch26.github.io/scoreboard/index.html"
+  },
+  {
+    "revision": "e72f3386a29ef92f810d",
+    "url": "/motch26.github.io/scoreboard/static/css/main.6e3e2e6d.chunk.css"
+  },
+  {
+    "revision": "4399f0acafa4950071ec",
+    "url": "/motch26.github.io/scoreboard/static/js/2.6e002ccb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/motch26.github.io/scoreboard/static/js/2.6e002ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72f3386a29ef92f810d",
+    "url": "/motch26.github.io/scoreboard/static/js/main.0ab8f83a.chunk.js"
+  },
+  {
+    "revision": "9659f0cf6e691e79129b",
+    "url": "/motch26.github.io/scoreboard/static/js/runtime-main.465ee389.js"
+  }
+]);
